857f9151cc79ae87b462165a35c9c1e3fb877178
Jimmy Hu <jimmyhu.co@gmail.com>
2017-11-30 12:20:46 +0800

    Using Data.List.Lazy

--- src/Sumdigpow.purs
+++ src/Sumdigpow.purs
@@ -2,28 +2,19 @@ module Codewars.G964.Sumdigpow where
 
 import Data.Char.Unicode (digitToInt)
 import Data.Int (pow)
-import Data.List (List(Nil), filter, zipWith, (..), (:))
+import Data.List.Lazy (filter, iterate, zipWith, (..))
+import Data.List.Lazy.Types (List, nil)
 import Data.Maybe (fromMaybe)
 import Data.String.Yarn (toChars)
 import Data.Traversable (sum, traverse)
-import Prelude (flip, show, ($), (+), (<<<), (==))
+import Prelude (add, flip, show, ($), (<<<), (==))
 
 -- Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
 -- https://www.codewars.com/kata/5626b561280a42ecc50000d1
-sumDigPow' :: Int -> Int -> List Int
-sumDigPow' a b = filter sdp (a..b)
-
 sumDigPow :: Int -> Int -> List Int
 sumDigPow a b = filter f (a..b)
   where
-    f n = (_ == n) <<< sum <<< zipWith (flip pow) (1..100) <<< toDigits $ n
-
-sdp :: Int -> Boolean
-sdp x = x == sdp' (toDigits x) 1
-  where
-    sdp' :: List Int -> Int -> Int
-    sdp' Nil count = 0
-    sdp' (y:ys) count = pow y count + sdp' ys (count+1)
+    f n = (_ == n) <<< sum <<< zipWith (flip pow) (iterate (add 1) 1) <<< toDigits $ n
 
 toDigits :: Int -> List Int
-toDigits x = fromMaybe Nil $ traverse digitToInt $ toChars $ show x
+toDigits x = fromMaybe nil $ traverse digitToInt $ toChars $ show x
--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,11 @@
 module Test.Main where
 
 import Codewars.G964.Sumdigpow
+import Data.List.Lazy.Types (nil, (:))
 
+import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.AVar (AVAR)
 import Control.Monad.Eff.Console (CONSOLE)
-import Data.List (List(Nil), (:))
 import Prelude (Unit, discard)
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
@@ -21,10 +21,10 @@ main ::
 main = runTest do
   suite "sync code" do
     test "sumDigPow" do
-      Assert.equal (sumDigPow 1 10) (1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : Nil)
-      Assert.equal (sumDigPow 1 100) (1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : 89 : Nil)
-      Assert.equal (sumDigPow 10 100) (89 : Nil)
-      Assert.equal (sumDigPow 90 100) (Nil)
-      Assert.equal (sumDigPow 90 150) (135 : Nil)
-      Assert.equal (sumDigPow 50 150) (89: 135 : Nil)
-      Assert.equal (sumDigPow 10 150) (89: 135 : Nil)
+      Assert.equal (sumDigPow 1 10) (1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : nil)
+      Assert.equal (sumDigPow 1 100) (1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : 89 : nil)
+      Assert.equal (sumDigPow 10 100) (89 : nil)
+      Assert.equal (sumDigPow 90 100) (nil)
+      Assert.equal (sumDigPow 90 150) (135 : nil)
+      Assert.equal (sumDigPow 50 150) (89: 135 : nil)
+      Assert.equal (sumDigPow 10 150) (89: 135 : nil)
